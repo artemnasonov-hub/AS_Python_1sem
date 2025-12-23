b2d37b69739e3b096dbc3db99b62aeac0a029f28
artemnasonov-hub <artemnasonov263@gmail.com>
2025-12-23 18:44:18 +0300

    Update main.py

--- 2.3lab/main.py
+++ 2.3lab/main.py
@@ -1,2 +1,49 @@
 if __name__ == "__main__":
     pass
+
+
+
+
+
+
+def MtrProd(A, B):
+    rows_A = len(A)
+    cols_A = len(A[0])
+    cols_B = len(B[0])
+    result = []
+    for i in range(rows_A):
+        row = []
+        for j in range(cols_B):
+            row.append(0)
+        result.append(row)
+    for i in range(rows_A):
+        for j in range(cols_B):
+            for k in range(cols_A):
+                result[i][j] += A[i][k] * B[k][j]
+
+    return result
+
+
+A = [
+    [5, 2],
+    [7, 4] ]
+
+
+B = [
+    [1, 2],
+    [3, 4] ]
+
+
+C = MtrProd(A, B)
+
+print("A:")
+for row in A:
+    print(row)
+
+print("\n B:")
+for row in B:
+    print(row)
+
+print("\nПроизведение :")
+for row in C:
+    print(row)
