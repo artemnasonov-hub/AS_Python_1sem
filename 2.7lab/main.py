c6e671ed9bc277134ef2418181d3fb076af48e10
artemnasonov-hub <artemnasonov263@gmail.com>
2025-12-23 16:49:23 +0300

    Update main.py

--- 2.7lab/main.py
+++ 2.7lab/main.py
@@ -1,2 +1,15 @@
 if __name__ == "__main__":
     pass
+
+
+
+file = open("input.txt", "r", encoding="utf-8")
+lines = file.readlines()
+file.close()
+file = open("input.txt", "w", encoding="utf-8")
+
+for line in lines:
+    file.write(line)
+    if line == "\n":
+        file.write(line)
+file.close()
