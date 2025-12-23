7d105e02a62dfc6f511ae6598d65e43a2fe4c9b5
artemnasonov-hub <artemnasonov263@gmail.com>
2025-12-23 21:25:27 +0300

    Update main.py

--- 2.8lab/main.py
+++ 2.8lab/main.py
@@ -1,9 +1,6 @@
 if __name__ == "__main__":
     pass
-
-
 file = open("data.txt", "r", encoding="utf-8")
-
 line = file.readline()
 if line != "":
     parts = line.split()
@@ -11,12 +8,9 @@ if line != "":
     for line in file:
         parts = line.split()
         year = int(parts[2])
-
         if year < min_year:
             min_year = year
-
     print("Наименьший год:", min_year)
 else:
     print("Файл пустой")
-
 file.close()
