d7fd1462ae6faf72db5a30c202f2c1bd9a4ca3dc
artemnasonov-hub <artemnasonov263@gmail.com>
2025-12-23 18:33:40 +0300

    Update main.py

--- 2.2lab/main.py
+++ 2.2lab/main.py
@@ -1,2 +1,24 @@
 if __name__ == "__main__":
     pass
+
+
+
+
+
+def RectPS(x1, y1, x2, y2):
+    w = abs(x2 - x1)
+    h = abs(y2 - y1)
+    P = 2 * (w + h)
+    S = w * h
+    return P, S
+
+
+x1 = int(input("Введите x1: "))
+y1 = int(input("Введите y1: "))
+x2 = int(input("Введите x2: "))
+y2 = int(input("Введите y2: "))
+
+perimeter, square = RectPS(x1, y1, x2, y2)
+
+print("Периметр прямоугольника:", perimeter)
+print("Площадь прямоугольника:", square)
