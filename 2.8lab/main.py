89b8c8f291493b6892c9ab2abef1594f1f8ce8a3
artemnasonov-hub <artemnasonov263@gmail.com>
2025-12-23 19:18:56 +0300

    Update main.py

--- 2.8lab/main.py
+++ 2.8lab/main.py
@@ -2,43 +2,21 @@ if __name__ == "__main__":
     pass
 
 
-f = open("f.txt", "w", encoding="utf-8")
-f.write("1\n-3\n4\n-2\n5\n-6\n")
-f.close()
+file = open("data.txt", "r", encoding="utf-8")
 
-f = open("f.txt", "r", encoding="utf-8")
-numbers = []
+line = file.readline()
+if line != "":
+    parts = line.split()
+    min_year = int(parts[2])
+    for line in file:
+        parts = line.split()
+        year = int(parts[2])
 
-for line in f:
-    numbers.append(int(line))
+        if year < min_year:
+            min_year = year
 
-f.close()
+    print("Наименьший год:", min_year)
+else:
+    print("Файл пустой")
 
-positive = []
-negative = []
-
-for num in numbers:
-    if num > 0:
-        positive.append(num)
-    else:
-        negative.append(num)
-
-
-g = open("g.txt", "w", encoding="utf-8")
-
-for i in range(len(positive)):
-    g.write(str(positive[i]) + "\n")
-    g.write(str(negative[i]) + "\n")
-
-g.close()
-
-
-print("Содержимое файла f.txt:")
-f = open("f.txt", "r", encoding="utf-8")
-print(f.read())
-f.close()
-
-print("Содержимое файла g.txt:")
-g = open("g.txt", "r", encoding="utf-8")
-print(g.read())
-g.close()
+file.close()
