99c2bbeb83bdaf3357a929283834cdb0708fe52f
artemnasonov-hub <artemnasonov263@gmail.com>
2025-12-23 19:12:31 +0300

    Update main.py

--- 2.7lab/main.py
+++ 2.7lab/main.py
@@ -7,14 +7,14 @@ if __name__ == "__main__":
 
 
 K = int(input("Введите номер строки K: "))
-file = open("text.txt", "r", encoding="utf-8")
+file = open("te.txt", "r", encoding="utf-8")
 lines = file.readlines()
 file.close()
 
 if K >= 1 and K <= len(lines):
     del lines[K - 1]
 
-file = open("text.txt", "w", encoding="utf-8")
+file = open("te.txt", "w", encoding="utf-8")
 for line in lines:
     file.write(line)
 file.close()
