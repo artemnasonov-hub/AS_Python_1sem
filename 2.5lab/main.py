10ab467e40156eddb47e011f5096195698651019
artemnasonov-hub <artemnasonov263@gmail.com>
2025-12-23 16:34:59 +0300

    Update main.py

--- 2.5lab/main.py
+++ 2.5lab/main.py
@@ -1,2 +1,15 @@
 if __name__ == "__main__":
     pass
+
+
+
+
+def sum_of_digits(n):
+    if n < 10:
+        return n
+    return n % 10 + sum_of_digits(n // 10)
+
+
+# Проверка работы функции
+N = int(input("Введите натуральное число: "))
+print("Сумма цифр:", sum_of_digits(N))
