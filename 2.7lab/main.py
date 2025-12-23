72844fe52400658fc864f41b6d7c1e4ed287eb47
artemnasonov-hub <artemnasonov263@gmail.com>
2025-12-23 19:08:00 +0300

    Update main.py

--- 2.7lab/main.py
+++ 2.7lab/main.py
@@ -3,3 +3,20 @@ if __name__ == "__main__":
 
 
 
+
+
+
+K = int(input("Введите номер строки K: "))
+file = open("text.txt", "r", encoding="utf-8")
+lines = file.readlines()
+file.close()
+
+if K >= 1 and K <= len(lines):
+    del lines[K - 1]
+
+file = open("text.txt", "w", encoding="utf-8")
+for line in lines:
+    file.write(line)
+file.close()
+
+print("Всё готово")
